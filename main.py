f665bd71d08f48a23028a64ac9cff6895f68d07a
Aakash Vijeta <aakashvijeta2@gmail.com>
2026-03-20 07:38:46 +0530

    lock CORS for production domain

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://f1.aakashvijeta.me"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
