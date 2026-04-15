9e08865acd08f2226bbc65d54dab2e3d0a20773c
Aakash Vijeta <aakashvijeta2@gmail.com>
2026-04-16 00:25:40 +0530

    api base change

--- f1-frontend/src/constants/drivers.js
+++ f1-frontend/src/constants/drivers.js
@@ -45,10 +45,10 @@ export const DRIVERS = {
 
 export const MEDALS = ["#F5C518", "#A8A9AD", "#CD7F32"];
 export const MEDAL_LABELS = ["WINNER", "2ND PLACE", "3RD PLACE"];
-// export const API_BASE = import.meta.env.VITE_API_URL
+export const API_BASE = import.meta.env.VITE_API_URL
 
 // testing
-export const API_BASE = "http://127.0.0.1:8000";
+// export const API_BASE = "http://127.0.0.1:8000";
 
 // Get driver info, fallback for unknowns
 export const gd = (n) =>
